239b6e3a86d1f338bcebdd552733fd2afb7726a6
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2021-10-18 13:33:55 +0200

    Proyecto creado

---
+++ models/ticket-control.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const fs = require('fs')
+
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero
+    this.escritorio = escritorio
+  }
+}
+
+class TicketControl {
+  constructor() {
+    this.hoy = new Date().getDate()
+    this.tickets = []
+    this.ultimo = 0
+    this.ultimos4 = []
+
+    this.init()
+  }
+
+  get toJson() {
+    return {
+      hoy: this.hoy,
+      tickets: this.tickets,
+      ultimo: this.ultimo,
+      ultimos4: this.ultimos4
+    }
+  }
+
+  init() {
+    const {hoy, tickets, ultimo, ultimos4} = require('../db/data.json')
+    if (hoy === this.hoy) {
+      this.tickets = tickets
+      this.ultimo = ultimo
+      this.ultimos4 = ultimos4
+    } else {
+      this.guardarDB()
+    }
+  }
+
+  guardarDB() {
+    const dbPath = path.join(__dirname, '../db/data.json')
+    fs.writeFileSync(dbPath, JSON.stringify(this.toJson))
+  }
+
+  siguiente() {
+    this.ultimo += 1
+
+    this.tickets.push(new Ticket(this.ultimo, null))
+
+    this.guardarDB()
+    return 'Ticket ' + this.ultimo
+  }
+
+  atenderTicket(escritorio) {
+    // No hay tickets
+    if (this.tickets.length === 0) {
+      return null
+    }
+
+    const ticket = this.tickets.shift()
+    ticket.escritorio = escritorio
+
+    this.ultimos4.unshift(ticket)
+    if(this.ultimos4.length > 4) {
+      this.ultimos4.splice(-1, 1)
+    }
+
+    this.guardarDB()
+
+    return ticket
+  }
+}
+
+module.exports = TicketControl
